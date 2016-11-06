95012a213406194a2b2f31cc387403c95b79b24b
liuyangming <liuyangming@gmail.com>
2016-11-06 16:13:33 +0800

    * refactor

--- bytetcc-core/src/main/java/org/bytesoft/bytetcc/CompensableContextImpl.java
+++ bytetcc-core/src/main/java/org/bytesoft/bytetcc/CompensableContextImpl.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this distribution; if not, see <http://www.gnu.org/licenses/>.
  */
-package org.bytesoft.bytetcc.supports;
+package org.bytesoft.bytetcc;
 
 import java.io.Serializable;
 
