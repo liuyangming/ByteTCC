6548c04d13e63546e8b0fda9395e38f9d408b1ac
wujun8 <wujun8@hotmail.com>
2018-04-17 16:50:00 +0800

    default Propagation.REQUIRED not null

--- bytetcc-supports/src/main/java/org/bytesoft/bytetcc/supports/spring/CompensableMethodInterceptor.java
+++ bytetcc-supports/src/main/java/org/bytesoft/bytetcc/supports/spring/CompensableMethodInterceptor.java
@@ -164,7 +164,7 @@ public class CompensableMethodInterceptor
 
 			if (transactional != null && compensable != null && transaction != null) {
 				Propagation propagation = transactional == null ? null : transactional.propagation();
-				if (propagation == null) {
+				if (Propagation.REQUIRED.equals(propagation)) {
 					compensable.registerCompensable(invocation);
 				} else if (Propagation.REQUIRES_NEW.equals(propagation)) {
 					compensable.registerCompensable(invocation);
