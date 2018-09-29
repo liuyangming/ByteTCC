f788fc2c76ca1a25174b527ff28316b63dab5cf0
liuyangming <liuyangming@gmail.com>
2018-09-29 18:31:03 +0800

    update LocalXADataSourcePostProcessor.java

--- bytetcc-supports/src/main/java/org/bytesoft/bytetcc/supports/resource/LocalXADataSourcePostProcessor.java
+++ bytetcc-supports/src/main/java/org/bytesoft/bytetcc/supports/resource/LocalXADataSourcePostProcessor.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this distribution; if not, see <http://www.gnu.org/licenses/>.
  */
-package org.bytesoft.bytetcc.supports.boot;
+package org.bytesoft.bytetcc.supports.resource;
 
 import javax.sql.DataSource;
 import javax.transaction.TransactionManager;
